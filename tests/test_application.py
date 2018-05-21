da6d7a78c14e733dde4b2395764d9270a427f10a
Robin Suter <suter.robin@gmail.com>
2018-05-21 16:51:03 +0200

    update tests

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,32 +1,27 @@
 import pytest
 from .context import backend
 
+
 @pytest.fixture
 def test_client():
     return backend.app.test_client()
 
 
-def test_available_ratings(test_client):
-    response = test_client.get('/api/availableRatings')
-
+def test_available_gradings(test_client):
+    response = test_client.get('/api/gradings')
     assert response.status_code == 200
 
 
-def test_available_ratings_filtered(test_client):
-    response = test_client.get('/api/availableRatings?typeOfDay=Wochentag')
+def test_available_gradings_filtered(test_client):
+    response = test_client.get('/api/gradings?typeOfDay=Saturday')
     assert response.status_code == 200
 
 
 def test_available_days(test_client):
-    response = test_client.get('/api/availableDays')
+    response = test_client.get('/api/typesOfDays')
     assert response.status_code == 200
 
 
-def test_get_rating(test_client):
-    response = test_client.get('/api/rating/0')
-    assert response.status_code == 200
-
-
-def test_get_rating_nonexistent(test_client):
+def test_get_grading_nonexistent(test_client):
     response = test_client.get('/api/rating/1000')
     assert response.status_code == 404
